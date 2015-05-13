9ff9edb40a94cb7aaa05ba06c94980c5fe282ed2
Dennis Schubert <mail@dennis-schubert.de>
2015-05-13 04:57:26 +0200

    Basic (yet not functional) list of CA certificates

--- fx-catrustmanager/src/content/js/main.js
+++ fx-catrustmanager/src/content/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const {classes: Cc, interfaces: Ci} = Components;
 
 class CACertificates {
@@ -11,20 +13,58 @@ class CACertificates {
     while(enumerator.hasMoreElements()) {
       let cert = enumerator.getNext();
       cert.QueryInterface(Ci.nsIX509Cert);
-      certs.push(cert);
+      cert = Object.create(cert);
+
+      cert.tableTitle = [
+        cert.issuerCommonName || cert.issuerOrganizationUnit || cert.organization,
+        cert.commonName
+      ].filter(function(value) { return value; })
+       .join(" - ");
+
+       certs.push(cert);
     }
+    certs.sort(function(a, b) {
+      if (a.tableTitle.toLowerCase() > b.tableTitle.toLowerCase()) { return 1; }
+      if (a.tableTitle.toLowerCase() < b.tableTitle.toLowerCase()) { return -1; }
+      return 0;
+    });
     return certs;
+  }
+
+  getCertByNickname(nickname) {
+    return this._certDB.findCertByNickname(null, nickname);
   }
 }
 
+class UI {
+  constructor(certs) {
+    this._certs = certs;
+  }
+
+  populateTable() {
+    let template = Handlebars.compile(document.getElementById("certificate-template").innerHTML),
+        tableHtml = "";
+
+    this._certs.getCerts().forEach(function(cert) {
+      tableHtml += template(cert);
+    });
+    document.querySelector("tbody").innerHTML = tableHtml;
+
+    // TODO replace me with something useful.
+    for(let toggler of document.querySelectorAll("tr")) {
+      toggler.addEventListener("click", (e) => {
+        let certNickname = e.target.parentNode.parentNode.dataset.nickname,
+            cert = this._certs.getCertByNickname(certNickname),
+            bit = e.target.dataset.bit;
+        console.dir({certNickname, cert, bit});
+      });
+    }
+  }
+}
+
+window.certs = new CACertificates();
+window.ui = new UI(certs);
+
 document.addEventListener("DOMContentLoaded", function() {
-  let certs = new CACertificates(),
-      template = Handlebars.compile(document.getElementById("certificate-template").innerHTML),
-      tableHtml = "";
-
-  certs.getCerts().forEach(function(cert) {
-    tableHtml += template({nickname: cert.nickname, name: cert.commonName});
-  });
-
-  document.querySelector("tbody").innerHTML = tableHtml;
+  ui.populateTable();
 });
