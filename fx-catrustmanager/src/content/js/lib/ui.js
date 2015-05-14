32fb5fbdec5a018217e70fed8d2904a7c8c8789d
Dennis Schubert <mail@dennis-schubert.de>
2015-05-14 05:20:02 +0200

    Move classes to own files

---
+++ fx-catrustmanager/src/content/js/lib/ui.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class UI {
+  constructor(certs) {
+    this._certs = certs;
+    this._certificateRowTemplate = Handlebars.compile(document.getElementById("certificate-row-template").innerHTML);
+  }
+
+  populateTable() {
+    let table = document.querySelector("tbody");
+    this._certs.getCerts().forEach((cert) => {
+      table.appendChild(this.getCertificateRowDOMObject(cert));
+    });
+  }
+
+  getCertificateRowDOMObject(cert) {
+    let row = document.createElement("tr");
+    row.innerHTML = this._certificateRowTemplate(cert);
+    row.dataset.nickname = cert.nickname;
+
+    for(let toggler of row.querySelectorAll(".toggleButton")) {
+      toggler.addEventListener("click", (e) => {
+        let certNickname = e.target.parentNode.parentNode.dataset.nickname,
+            cert = this._certs.getCertByNickname(certNickname),
+            bit = e.target.dataset.bit;
+        e.preventDefault();
+        console.dir({certNickname, cert, bit});
+      });
+    }
+
+    return row;
+  }
+}
